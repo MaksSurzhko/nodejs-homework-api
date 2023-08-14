0a410f509a1f55926513a0e87a1c379d94affcdf
MaksSurzhko <117912210+MaksSurzhko@users.noreply.github.com>
2023-08-14 13:51:14 +0300

    upd homework

--- controllers/contacts/update.js
+++ controllers/contacts/update.js
@@ -1,26 +1,40 @@
 const operations = require("../../models/contacts");
 const Joi = require("joi");
+
 const schema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().required(),
-  phone: Joi.string().required(),
+ phone: Joi.string().required(),
 });
+
 const update = async (req, res) => {
   const { contactId } = req.params;
   const body = req.body;
   const { error } = schema.validate(body);
-    if (error) {
-    const error = new Error ('missing fields')
-    error.status = 400;
-    throw error;
+
+  if (!body.name && !body.email && !body.phone) {
+    const missingFieldsError = new Error("missing fields");
+    missingFieldsError.status = 400;
+    throw missingFieldsError;
   }
+
+  if (error) {
+    const missingField = error.details[0].context.label;
+    const errorMessage = `Missing required ${missingField} field`;
+    const customError = new Error(errorMessage);
+    customError.status = 400;
+    throw customError;
+  }
+
   const updatedContact = await operations.updateContact(contactId, body);
+
   if (!updatedContact) {
-    const error = new Error("Not found");
-    error.status = 404;
-    throw error;
+    const notFoundError = new Error("Not found");
+    notFoundError.status = 404;
+    throw notFoundError;
   }
-  res.json(updatedContact);  
+
+  res.json(updatedContact);
 };
 
 module.exports = update;
