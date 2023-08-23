7571610d9d8339347b98d9c5fd459d93b6a37a50
MaksSurzhko <117912210+MaksSurzhko@users.noreply.github.com>
2023-08-23 18:49:30 +0300

    upd hw

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
-const app = require('./app')
+const app = require("./app");
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
- console.log("Homework")
+mongoose
+  .connect("mongodb+srv://Maks:maks2002@cluster01.smaassk.mongodb.net/", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Database connection successful");
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  })
+  .catch((err) => {
+    console.log(err.message);
+    process.exit(1);
+  });
