aecabc128cc3fb979355ee25b9617d4cdce4af13
MaksSurzhko <117912210+MaksSurzhko@users.noreply.github.com>
2023-08-29 12:16:15 +0300

    hw-4-1

--- app.js
+++ app.js
@@ -1,7 +1,35 @@
+// const express = require("express");
+// const logger = require("morgan");
+// const cors = require("cors");
+// const contactsRouter = require("./routes/api/contacts");
+// require("dotenv").config();
+
+// const app = express();
+
+// const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+// app.use(logger(formatsLogger));
+// app.use(cors());
+// app.use(express.json());
+
+// app.use("/api/contacts", contactsRouter);
+
+// app.use((req, res) => {
+//   res.status(404).json({ message: "Not found" });
+// });
+
+// app.use((err, req, res, next) => {
+//   const { status = 500, message = "Server error" } = err;
+//   res.status(status).json({ message });
+// });
+// module.exports = app;
+
+
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 const contactsRouter = require("./routes/api/contacts");
+const usersRouter = require("./routes/api/users");
 require("dotenv").config();
 
 const app = express();
@@ -12,6 +40,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/users", usersRouter);
 app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
