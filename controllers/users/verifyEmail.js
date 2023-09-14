34a18daf20a1e3c5c56556084b6782580dad74ca
MaksSurzhko <117912210+MaksSurzhko@users.noreply.github.com>
2023-09-14 20:37:46 +0300

    upddd

--- controllers/users/verifyEmail.js
+++ controllers/users/verifyEmail.js
@@ -1,25 +1,6 @@
 const { User } = require("../../models/user");
 const HttpError = require("./../../helpers/HttpError");
 
-// const verifyEmail = async (req, res) => {
-//   const { verificationToken } = req.params;
-//   const user = await User.findOne({ verificationToken });
-//   if (!user) {
-//     HttpError(404, "User not found");
-//   }
-
-//   await User.findByIdAndUpdate(user._id, {
-//     verificationToken: null,
-//     verify: true,
-//   });
-
-//   res.json({
-//     message: "Verification successful",
-//   });
-// };
-
-// module.exports = verifyEmail;
-
 const verifyEmail = async (req, res) => {
   const { verificationToken } = req.params;
   const user = await User.findOne({ verificationToken });
